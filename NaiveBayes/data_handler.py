2e31ef82c28f05833f41da4cd6eb62cdeee84ba0
Prena-Lalwani <prenalalwani556@gmail.com>
2025-08-26 11:40:50 +0500

    Abstract of model

---
+++ NaiveBayes/data_handler.py
@@ -0,0 +1,16 @@
+# Sample movie reviews
+texts = [
+    "I loved this movie, it was amazing!",
+    "What a terrible movie, I hated it.",
+    "It was okay, not the best but not bad either.",
+    "Absolutely fantastic! Would watch again.",
+    "Worst movie I have ever seen.",
+    "Pretty good film, I enjoyed it.",
+    "Awful, do not waste your time.",
+    "Excellent acting and great story.",
+    "Not great, but not too bad.",
+    "I really liked it, a very nice movie."
+]
+
+# Labels: 1 = Positive, 0 = Negative
+labels = [1, 0, 1, 1, 0, 1, 0, 1, 1, 1]
